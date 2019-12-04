75629051859901dda8cfa3b74509fa6d0967f189
Alec Alfiler <alec.alfiler@gmail.com>
2019-12-04 05:39:49 -0800

    Added burst effect upon death..
    also added restart, fixed gravity so it jumps roughly 1 block, changed level structure accordingly.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+    bool gameHasEnded = false; //tracker to know if game has been failed or completed.
+    public GameObject completeGameUI; //upon winning, this will be enabled.
+    public GameObject titleScreen; 
+
+    public void playGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1); //advance to game scene from title screen.
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EndGame() 
     {
+        if (gameHasEnded == false) //prevents constant restarting after reaching a certain circumstance (loss/win)
+        {
+            gameHasEnded = true;
+            Invoke("Restart" , 1f);
+        }
+        else { //added an if statement since game would be at false for "Play Again" button.
+            Restart();
+
+        }
+    }
+
+    public void winGame()
+    {
+        if (gameHasEnded == false)
+        {
+            animator.SetTrigger("CompleteGame"); //triggers the animation which fades the credits in.
+            gameHasEnded = true;
+            completeGameUI.SetActive(true);
+        }
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("Application Shutting Down..");
+        Application.Quit(); //quits application upon click, only works for built application.
+    }
+
+    void Restart()
+    {
+        //animator.SetTrigger("FadeOut");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         
     }
+    
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    //bursted cube values
+    public float cubeSize = 0.2f;
+    public int cubes = 5;
+    public float burstForce = 50f;
+    public float burstRadius = 4f;
+    public float burstUpward = 0.4f;
+    
+    float cubesPivotDistance;
+    Vector3 cubesPivot;
+
+    public GameObject piecePrefab;
     public Rigidbody player;
     public bool grounded;
-    public Respawn respawnPoint;
 
     private void Start()
     {
-        player.AddForce(5, 0, 0);
+       // player.AddForce(5, 0, 0);
         player = GetComponent<Rigidbody>();
+
+        cubesPivotDistance = cubeSize * cubes / 2;
+        cubesPivot = new Vector3(cubesPivotDistance, cubesPivotDistance, cubesPivotDistance);
     }
 
     // Update is called once per frame
@@ -28,6 +41,7 @@ public class PlayerMovement : MonoBehaviour
             Quaternion rotate = transform.rotation;
             rotate.z = Mathf.Round(rotate.z / 90) * 90;
             transform.rotation = rotate;
+            
             //jump
             if (Input.GetKey("space") && grounded == true)
             {
@@ -37,11 +51,12 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
+            //rotate back
             transform.Rotate(Vector3.back * 5f);
         }
     }
-
-    public void OnCollisionEnter(Collision collision)
+    // =========== COLLISIONS ==============
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Floor")
         {
@@ -49,11 +64,10 @@ public class PlayerMovement : MonoBehaviour
         }
         if (collision.collider.tag == "Obstacle")
         {
-            Debug.Log("We hit something");
-            respawnPoint.RestartGame();
+            burst();
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
-
 
     private void OnCollisionExit(Collision collision)
     {
@@ -61,5 +75,52 @@ public class PlayerMovement : MonoBehaviour
         {
             grounded = false;
         }
+    }
+
+    // ============= BURST THE CUBE INTO PIECES =============
+    public void burst()
+    {
+        // Destroy(player.gameObject);
+        gameObject.SetActive(false);
+       
+        for (int x = 0; x < cubes; x++)
+        {
+            for (int y = 0; y < cubes; y++)
+            {
+                for (int z = 0; z < cubes; z++)
+                {
+                    createPiece(x, y, z);
+                }
+            }
+        }
+        Vector3 explosionPos = transform.position; //get explosion position
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, burstRadius);  //get colliders at position 
+        foreach (Collider hit in colliders)  //add explosion force
+        {
+            //get rigidbody from collider object
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                //add explosion force to rb
+                rb.AddExplosionForce(burstForce, transform.position, burstRadius, burstUpward);
+            }
+        }
+    }
+
+
+    // ============ CREATE THE PIECES =============
+    void createPiece(int x, int y, int z) 
+    {
+        //create piece
+        GameObject piece;
+        piece = Instantiate(piecePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+        //set piece position and scale
+        piece.transform.position = transform.position + new Vector3(cubeSize * x, cubeSize * y, cubeSize * z) - cubesPivot;
+        piece.transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
+
+        //add rigidbody and set mass
+        piece.AddComponent<Rigidbody>();
+        piece.GetComponent<Rigidbody>().mass = cubeSize;
     }
 }
