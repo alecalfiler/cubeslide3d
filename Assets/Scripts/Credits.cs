f60b357b54a888ee7a2eacaf4241085e737f535a
Angelo Cabading <angelocabading@yahoo.com>
2019-12-10 11:59:26 -0800

    Finish line fix

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    public void Play()
+    {
+        GetComponent<Animation>();
+        StartCoroutine(LoadAfterAnim());
+    }
+
+    public IEnumerator LoadAfterAnim()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Singleton : MonoBehaviour
+{
+    private static Singleton instance = null;
+
+
+    public static Singleton Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        int buildIndex = currentScene.buildIndex;
+
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode mode)
+    {
+        if (scene.name == "MainMenu")
+        {
+            Destroy(gameObject);
+            Debug.Log("I am inside the if statement");
+        }
+    }
+}
+
